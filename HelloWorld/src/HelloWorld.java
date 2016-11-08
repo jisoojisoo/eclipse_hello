89354fcb2707ada8fc07afcc323dd49e468e08a5
HH305- <HH305-@HH306-31>
2016-11-08 13:45:27 +0900

    change hotfix branch.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
         System.out.println("HelloWorld");
         System.out.println("Hello everyone");
+        System.out.println("Hello hotfix");
 	}
 
 }
